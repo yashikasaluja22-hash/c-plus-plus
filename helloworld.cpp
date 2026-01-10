5080008b68c1424865d7f2d4d1205991d0be64f3
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-10 13:56:11 +0530

    new file

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+std::cout << "I like pizza!" << std::endl;
+std::cout << "It's really good!" << '\n';
+//here std means standard and cout means character output
+// << std::endl is for standard end line means to end the line or u can use << '\n' too
+//but endl avoids output buffer.
+return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+int main() {
+int x; //declaration
+x = 5; //assignment
+int y = 6;
+int sum = x + y;
+std::cout << x << '\n';
+std::cout << y << '\n';
+std::cout << sum << '\n';
+//int used for integers and double used for decimal values
+double gpa = 9.5;
+double price = 45.55, total = 8.800;
+std::cout << gpa << '\n';
+std::cout << price << '\n';
+std::cout << total << '\n';
+
+//char for single character
+char grade = 'A';
+char initial = 'Y';
+char currency = '$';
+std::cout << grade << '\n';
+std::cout << initial << '\n';
+//for boolean 
+bool student = true;
+bool power = false;
+std::cout << student << '\n';
+//string = sequence of text
+
+std::string name = "Brocode";
+std::string day = "Friday";
+std::string address = "123 fake st.";
+std::cout << "Hello" << name << '\n';
+std::cout << "You live at" << address << '\n'; 
+
+
+
+
+return 0;
+}
