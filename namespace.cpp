30d695a22edf0135a42e6ee654d48cfaa3a094d1
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-11 19:57:43 +0530

    new day new commit

--- namespace.cpp
+++ namespace.cpp
@@ -2,8 +2,17 @@
 //each entity needs a unique name. A Namespace allows for identically named entities as long as the
 // namespaces are different.
 #include <iostream>
+namespace first{
+    int x = 1;
+}
+namespace second{
+    int x = 2;
+}
+//u can write any random name instead of first or second
 int main() {
-
-
+int x = 0;
+std::cout << x << '\n';
+std::cout << first::x << '\n';
+std::cout << second::x << '\n';
 return 0;
 }      
