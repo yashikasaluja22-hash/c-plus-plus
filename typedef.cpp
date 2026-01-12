cf845afeee080ef4cbea50e05d42d8c070da47c7
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-12 12:21:06 +0530

    enola holmes

--- typedef.cpp
+++ typedef.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 typedef std::string text;
 typedef int num;
-
+//instead of typedef u can use "using" , just format is a bit different
+// better to use 'using'
+using num1 = int;
 int main() {
 text name = "Yashika";
 text surname = "Saluja";
@@ -12,11 +14,13 @@ text surname = "Saluja";
 num pin = 1212;
 num marks = 45;
 
+num1 items = 10;
+num1 rate = 55;
+
 std::cout << name << '\n';
 std::cout << surname << '\n';
 std::cout << "here is my pin number " << pin << '\n';
-std::cout << "marks: " << marks << '\n'; 
-
-
+std::cout << "marks: " << marks << '\n';
+std::cout << items;
 return 0;
 }
