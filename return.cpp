d4c550e3d45a48907641140835eb7a4978b557c4
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-18 21:33:16 +0530

    parantha

--- return.cpp
+++ return.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+double square(double length);
+
 int main() {
 // return = a keyword, return a value to the spot
 // where u called a the encompassing function
-
-
+double length = 5.0;
+double area = square(length);
+std::cout << "area of square: " << area << "cm\n";
 return 0;
+}
+double square(double length){
+    return length * length;
 }
