561c546f16c8d1151a79dd1449a3242e3198e8c4
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-20 19:48:50 +0530

    choley bhature

--- return.cpp
+++ return.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 double square(double length);
+double cubevolume(double length);
 
 int main() {
 // return = a keyword, return a value to the spot
@@ -7,8 +8,13 @@ int main() {
 double length = 5.0;
 double area = square(length);
 std::cout << "area of square: " << area << "cm\n";
+std::cout << square(length) << '\n';
+std::cout << "Volume is: " << cubevolume(length) << "cm^3\n";
 return 0;
 }
 double square(double length){
     return length * length;
+}
+double cubevolume(double length){
+    return length * length * length;
 }
