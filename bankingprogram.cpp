9e7e9177124601ace41d06890419c80962dd915e
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-01-25 09:40:08 +0530

    banking

--- bankingprogram.cpp
+++ bankingprogram.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <iomanip>
 using namespace std;
 void showBalance(double balance);
 double deposit();
@@ -21,8 +22,10 @@ switch(choice){
     case 1: showBalance(balance);
            break;
     case 2: balance =+ deposit()
+         showBalance(balance);
          break;
     case 3: balance =- withdraw(balance);
+         showBalance(balance);
          break;
     case 4: cout << "Thanks for visiting!!\n";
          break;
@@ -30,8 +33,43 @@ switch(choice){
           break;
 }
 }while(choice != 4);
-
-
-
 return 0;
+}
+//sometime double doesnt add decimal points so we use header file <iomanip>
+// and use setprecision(integer here)
+// this fixed will display our balance upto 3 decimal places.
+void showBalance(double balance){
+ cout << "Your balance is: $" << setprecision(3) << fixed << balance << '\n';
+}
+// agar tum function ke andar variable ko intialise kar rhe ho then no need ki uske parementer ke andar(brackets)  mein uska variable ka naam likho
+void deposit() {
+     double amount = 0;
+     cout << "How much money u wish to deposit: ";
+     cin >> amount;
+     //after this u need to show user its balance so we will include showbalance function in case 2 above 
+     if(amount > 0){
+          return amount;
+     }
+     else{
+          cout << "Not a valid amount!!\n";
+          return 0;
+     }
+}
+void withdraw(double balance){
+     double amount = 0;
+     cout << "How much money u wish to withdraw: ";
+     cin >> amount;
+
+     if(amount <= balance){
+          cout << '$ ' << amount << " has been withdrawn.\n";
+          return amount;
+     }
+     else if(amount < 0){
+          cout << "That's not a valid amount!!\n";
+          return 0;
+     }
+     else{
+          cout << "Insufficient funds!!\n";
+          return 0;
+     }
 }
